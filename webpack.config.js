d058b4ca995364b009703560bc64ee17b28b3ca7
zaus <jeremys.atwork@gmail.com>
2026-02-20 13:34:12 -0500

    build: moving static asset copy to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  entry: {
-    app: './src/renderer/app.js',
-    autocomplete: './src/renderer/autocomplete.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist/renderer'),
-    filename: '[name].bundle.js'
-  },
-  resolve: {
-    extensions: ['.js']
-  },
-  target: 'web'
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development';
+  const isDevelopment = mode === 'development';
+
+  return {
+      mode: mode,
+      devtool: isDevelopment ? 'source-map' : false,
+      entry: {
+        app: './src/renderer/app.js',
+        autocomplete: './src/renderer/autocomplete.js'
+      },
+      output: {
+        path: path.resolve(__dirname, 'dist/renderer'),
+        filename: '[name].bundle.js'
+      },
+      plugins: [
+        new CopyPlugin({
+          patterns: [
+            { from: 'src/renderer/index.html', to: 'index.html' },
+            { from: 'src/renderer/style.css', to: 'style.css' }
+          ]
+        })
+      ],
+      resolve: {
+        extensions: ['.js']
+      },
+      target: 'web'
+    };
 };
